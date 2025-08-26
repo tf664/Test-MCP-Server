8b791a2f8762d44044e147a386858a40fb4182b3
Til Feldkamp <til.feldkamp@babtec.de>
2025-08-26 14:34:43 +0200

    First MCP server

---
+++ ASPMCP/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class ASPTools
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"AHHHHHHH {message}";
+
+    [McpServerTool, Description("Get the current day of the week")]
+    public static string GetDayOfWeek()
+    {
+        var today = DateTime.Now.ToString("dddd");
+        return $"Today is the day: {today}.";
+    }
+}
